374dac28be0993bb410e1b91d7b89c8d23efcd00
Azimolab <65567817+Azimolab@users.noreply.github.com>
2021-06-13 19:55:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
 
-$(function(){
-  $('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' });
-
-  $(window).resize(function(){
-    $('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' });
-  });
-});
